40b790360ed0e8c6683be535297301ecac379916
Karolina Redden <dubajkaro@gmail.com>
2023-02-15 11:47:48 -0400

    add create table, add data and a loop

---
+++ courses.sql
@@ -0,0 +1,5 @@
+create table courses (
+  name text,
+  author text,
+  create_date date
+);
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$myPDO = new PDO('sqlite:/Users/karolinadubaj/lighthouse/personal_projects/sfh/php/Composer/module4.db');
+
+// $result = $myPDO->query("create table courses (
+//   name text,
+//   author text,
+//   create_date date
+// )");
+
+$result = $myPDO->query("insert into courses (name, author, create_date) values ('High performance PHP', 'karolina Redden', '03/29/2016')");
+
+$courses = $myPDO->query("select * from courses;");
+
+// echo var_dump($courses);
+
+// foreach ($courses as $key => $value) {
+//   echo var_dump($value);
+// }
+foreach ($courses as $course) {
+  print($course['author']);
+}
