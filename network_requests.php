c5b9424f851cc229aecaca61f64ab8987e19bab4
Karolina Redden <dubajkaro@gmail.com>
2023-02-15 15:05:36 -0400

    add endpoints

--- network_requests.php
+++ network_requests.php
@@ -2,6 +2,13 @@
 require __DIR__ . '/vendor/autoload.php';
 
 $global_start = microtime(true);
+
+$client = new GuzzleHttp\Client();
+request_endpoint($client, 'http://www.facebook.com');
+request_endpoint($client, 'http://www.google.com');
+request_endpoint($client, 'http://www.github.com');
+
+
 $global_end = microtime(true);
 
 echo "Full page loaded in: " . round($global_end - $global_start, 2) . " seconds";
