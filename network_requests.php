72eb346e69caf070cb6f5eeb139d5e025f0797ed
Karolina Redden <dubajkaro@gmail.com>
2023-02-15 14:54:43 -0400

    add a function to request endpoint

--- network_requests.php
+++ network_requests.php
@@ -5,3 +5,15 @@ $global_start = microtime(true);
 $global_end = microtime(true);
 
 echo "Full page loaded in: " . round($global_end - $global_start, 2) . " seconds";
+
+function request_endpoint($client, $url) {
+  echo "Requesting URL: $url...";
+  $start = microtime(true);
+  $response = $client->request('GET', $url);
+  $end = microtime(true);
+
+  echo $response->getBody();
+  echo "</br>Request completed in: " . round($end - $start, 2) . " 
+        seconds</br></br>";
+
+}
