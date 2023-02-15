937da758ae3e39f670941f18a7284c17798110d2
Karolina Redden <dubajkaro@gmail.com>
2023-02-15 13:29:42 -0400

    make function public

--- db.php
+++ db.php
@@ -1,10 +1,23 @@
 class DB {
-  private $handle;
-  private $logger;
+private $handle;
+private $logger;
 
-  public function __construct($logger) {
-    $this->handle = new PDO('sqlite:/Users/karolinadubaj/lighthouse/personal_projects/sfh/php/Composer/module4.db');;
-    $this->logger = $logger;
-  }
+public function __construct($logger) {
+$this->handle = new PDO('sqlite:/Users/karolinadubaj/lighthouse/personal_projects/sfh/php/Composer/module4.db');;
+$this->logger = $logger;
+}
+
+function read_query($db_handle, $query, $logger)
+{
+  $result = $db_handle->query($query);
+  $logger->info('Read query executed', ['query' => $query]);
+  return $result;
+}
+function write_query($db_handle, $query, $logger)
+{
+  $result = $db_handle->query($query);
+  $logger->notice('Write query executed', ['query' => $query]);
+  return $result;
+}
 
 }
